5ce44e6badc6e5f1b6898f8f4db84233e0fd2978
e-amce <adam.crow@gft.com>
2021-07-30 15:53:36 +0100

    Adding network/project/app-infra scripts

--- scripts/networks/networks.sh
+++ scripts/networks/networks.sh
@@ -1,16 +1,15 @@
-gcloud source repos clone gcp-environments --project=YOUR_CLOUD_BUILD_PROJECT_ID
-cd gcp-environments
+gcloud source repos clone gcp-networks --project=YOUR_CLOUD_BUILD_PROJECT_ID
 git checkout -b plan
-cp -RT ../terraform-example-foundation/2-environments/ .
+cp -RT ../terraform-example-foundation/3-networks/ .
 cp ../terraform-example-foundation/build/cloudbuild-tf-* .
 cp ../terraform-example-foundation/build/tf-wrapper.sh .
 chmod 755 ./tf-wrapper.sh
 git add .
 git commit -m 'Your message'
 git push --set-upstream origin plan
+git checkout -b production
+git push origin production
 git checkout -b development
 git push origin development
 git checkout -b non-production
 git push origin non-production
-git checkout -b production
-git push origin production
