a4668db5da1445062a4c7bf2e5338d0d3ff4d57c
e-amce <adam.crow@gft.com>
2021-08-02 22:02:25 +0100

    Update to app-infra

--- scripts/5-app-infra/app-infra.sh
+++ scripts/5-app-infra/app-infra.sh
@@ -13,6 +13,8 @@ CFT_FOLDER=./terraform-example-foundation
 [ -d $CFT_FOLDER ] && { echo "Removing past deployment file: $CFT_FOLDER"; rm -rf $CFT_FOLDER; } || echo "No past deployments found"
 git clone https://github.com/terraform-google-modules/terraform-example-foundation.git
 
+
+
 echo Checkout latest release
 cd ./terraform-example-foundation/
 git checkout ed164ba
@@ -24,7 +26,8 @@ GCP_POLICIES_FOLDER=./gcp-policies
 gcloud source repos clone gcp-policies --project=$YOUR_INFRA_PIPELINE_PROJECT_ID
 
 
-cd gcp-projects
+cd gcp-policies
+
 
 echo Copy review policies
 cp -R ../terraform-example-foundation/policy-library/ .
@@ -34,7 +37,7 @@ git add .
 git commit -m 'Your message'
 
 echo set and push to master
-git push --set-upstream origin master
+git push --set-upstream origin master --force
 cd ..
 
 echo Cloning gcp bu1-example-app GSR
@@ -52,12 +55,14 @@ cp ../terraform-example-foundation/build/cloudbuild-tf-* .
 cp ../terraform-example-foundation/build/tf-wrapper.sh .
 chmod 755 ./tf-wrapper.sh
 
+
+
 echo Removing unneeded bu1-development.auto.example.tfvars
 TF_EXAMPLE_VARS=./bu1-development.auto.example.tfvars
 [ -f $TF_EXAMPLE_VARS ] && { echo "Removing unneeded $TF_EXAMPLE_VARS file: $TF_EXAMPLE_VARS"; rm $TF_EXAMPLE_VARS; } || { echo "No $TF_EXAMPLE_VARS file found"; }
 
 echo Copying in needed bu1-development.auto.tfvars
-TF_VARS=../../scripts/4-projects/bu1-development.auto.tfvars
+TF_VARS=../../scripts/5-app-infra/bu1-development.auto.tfvars
 COPY_LOCATION=.
 [ -f $TF_VARS ] && { echo "Copying $TF_VARS to $COPY_LOCATION"; cp $TF_VARS $COPY_LOCATION; } || { echo "No $TF_VARS file found";  }
 
@@ -65,8 +70,8 @@ echo Removing unneeded bu1-non-production.auto.example.tfvars
 TF_EXAMPLE_VARS=./bu1-non-production.auto.example.tfvars
 [ -f $TF_EXAMPLE_VARS ] && { echo "Removing unneeded $TF_EXAMPLE_VARS file: $TF_EXAMPLE_VARS"; rm $TF_EXAMPLE_VARS; } || { echo "No $TF_EXAMPLE_VARS file found"; }
 
-echo Copying in needed bu1-non-production.auto.example.tfvars
-TF_VARS=../../scripts/4-projects/bu1-non-production.auto.example.tfvars
+echo Copying in needed bu1-non-production.auto.tfvars
+TF_VARS=../../scripts/5-app-infra/bu1-non-production.auto.tfvars
 COPY_LOCATION=.
 [ -f $TF_VARS ] && { echo "Copying $TF_VARS to $COPY_LOCATION"; cp $TF_VARS $COPY_LOCATION; } || { echo "No $TF_VARS file found";  }
 
@@ -75,26 +80,35 @@ TF_EXAMPLE_VARS=./bu1-production.auto.example.tfvars
 [ -f $TF_EXAMPLE_VARS ] && { echo "Removing unneeded $TF_EXAMPLE_VARS file: $TF_EXAMPLE_VARS"; rm $TF_EXAMPLE_VARS; } || { echo "No $TF_EXAMPLE_VARS file found"; }
 
 echo Copying in needed bu1-production.auto.example.tfvars
-TF_VARS=../../scripts/4-projects/bu1-production.auto.example.tfvars
+TF_VARS=../../scripts/5-app-infra/bu1-production.auto.example.tfvars
+COPY_LOCATION=.
+[ -f $TF_VARS ] && { echo "Copying $TF_VARS to $COPY_LOCATION"; cp $TF_VARS $COPY_LOCATION; } || { echo "No $TF_VARS file found";  }
+
+echo Removing unneeded common.auto.example.tfvars
+TF_EXAMPLE_VARS=./common.auto.example.tfvars
+[ -f $TF_EXAMPLE_VARS ] && { echo "Removing unneeded $TF_EXAMPLE_VARS file: $TF_EXAMPLE_VARS"; rm $TF_EXAMPLE_VARS; } || { echo "No $TF_EXAMPLE_VARS file found"; }
+
+echo Copying in needed common.auto.tfvars
+TF_VARS=../../scripts/5-app-infra/common.auto.tfvars
 COPY_LOCATION=.
 [ -f $TF_VARS ] && { echo "Copying $TF_VARS to $COPY_LOCATION"; cp $TF_VARS $COPY_LOCATION; } || { echo "No $TF_VARS file found";  }
 
 
 git add .
 git commit -m 'Your message'
-git push --set-upstream origin plan
-
-sleep 300
-
-git checkout -b development
-git push origin development
-
-sleep 300
-
-git checkout -b non-production
-git push origin non-production
-
-sleep 300
-
-git checkout -b production
-git push origin production
+git push --set-upstream origin plan --force
+#
+#sleep 300
+#
+#git checkout -b development
+#git push origin development --force
+#
+#sleep 300
+#
+#git checkout -b non-production
+#git push origin non-production --force
+#
+#sleep 300
+#
+#git checkout -b production
+#git push origin production --force
