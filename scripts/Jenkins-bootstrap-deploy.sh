c3fb109b9f885c5c13ade1aa538ba5e8a2a1a62a
alanzoabc <meallydavis@yahoo.co.uk>
2021-08-05 17:11:04 +0100

    Jenkins Script

---
+++ scripts/Jenkins-bootstrap-deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ./scripts/bootstrap
+rm terraform.example.tfvars && touch terraform.tfvars
+echo org_id = "\"$org_id"\" >> terraform.tfvars
+echo billing_account = "\"$billing_account"\" >> terraform.tfvars
+echo group_org_admins ="\"$group_org_admins"\" >> terraform.tfvars
+echo group_billing_admins ="\"$group_billing_admins"\" >> terraform.tfvars
+echo default_region = "\"$default_region"\" >> terraform.tfvars
+echo parent_folder = "\"$parent_folder"\" >> terraform.tfvars
+cat terraform.tfvars
+make bootstrap
+cd ../..
