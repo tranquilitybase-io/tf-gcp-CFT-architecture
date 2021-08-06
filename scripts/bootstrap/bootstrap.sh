939fb6e568a6fcfefeeb60404b4383d2f50e4122
obrienalaribe <obrienalaribe@gmail.com>
2021-08-06 14:19:08 +0100

    Update bootstrap.sh

--- scripts/bootstrap/bootstrap.sh
+++ scripts/bootstrap/bootstrap.sh
@@ -12,15 +12,16 @@ cd ./terraform-example-foundation/0-bootstrap/
 echo Removing unneeded terraform files
 rm terraform.example.tfvars
 
-echo Removing any past deployment terraform.tfvars files
-PAST_TF_VARS_FILE=./terraform.tfvars
-[ -f $PAST_TF_VARS_FILE ] && { echo "Removing past deployment file "; rm $PAST_TF_VARS_FILE; } || echo "No past deployment files found"
+# echo Removing any past deployment terraform.tfvars files
+# PAST_TF_VARS_FILE=./terraform.tfvars
+# [ -f $PAST_TF_VARS_FILE ] && { echo "Removing past deployment file "; rm $PAST_TF_VARS_FILE; } || echo "No past deployment files found"
 
-echo Copying in variable file
-TF_VARS_FILE=../../../scripts/bootstrap/terraform.tfvars
-[ -f $TF_VARS_FILE ] && { echo "Copying terraform.tfvars"; cp $TF_VARS_FILE .; } || { echo "can't find $TF_VARS_FILE"; exit 1; }
+# echo Copying in variable file
+# TF_VARS_FILE=../../../scripts/bootstrap/terraform.tfvars
+# [ -f $TF_VARS_FILE ] && { echo "Copying terraform.tfvars"; cp $TF_VARS_FILE .; } || { echo "can't find $TF_VARS_FILE"; exit 1; }
 
-echo Running terraform init/plan/apply
+echo "Running terraform init"
 terraform init
-terraform plan
-terraform apply -auto-approve
+echo "Running terraform plan"
+terraform plan -out cft-bootstrap-plan -var-file=landing_zone_params.json 
+terraform apply -auto-approve cft-bootstrap-plan
