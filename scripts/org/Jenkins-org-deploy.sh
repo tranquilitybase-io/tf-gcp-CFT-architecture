edf5911c17c7e3a3690d6a2cde166f84e47b5e49
alanzoabc <67590124+alanzoabc@users.noreply.github.com>
2021-08-06 10:07:06 +0100

    Update Jenkins-org-deploy.sh

--- scripts/org/Jenkins-org-deploy.sh
+++ scripts/org/Jenkins-org-deploy.sh
@@ -4,7 +4,7 @@ cd ./scripts/org
 rm terraform.example.tfvars env-variables-example.sh && touch terraform.tfvars env-variables.sh
 echo export CLOUD_BUILD_PROJECT_ID=prj-b-cicd >> env-variables.sh
 echo domains_to_allow = ["\"$domains_to_allow"\"] >> terraform.tfvars
-echo billing_data_users ="\"$billing_data_users "\" >> terraform.tfvars
+echo billing_data_users = "\"$billing_data_users"\" >> terraform.tfvars
 echo audit_data_users = "\"$audit_data_users"\" >> terraform.tfvars
 echo org_id = "\"$org_id"\" >> terraform.tfvars
 echo billing_account = "\"$billing_account"\" >> terraform.tfvars
@@ -12,7 +12,7 @@ echo terraform_service_account = "\"$terraform_service_account"\" >> terraform.t
 echo default_region = "\"$default_region"\" >> terraform.tfvars
 echo scc_notification_name = "\"$scc_notification_name"\" >> terraform.tfvars
 echo create_access_context_manager_access_policy = "false" >> terraform.tfvars
-echo parent_folder =  "\"$parent_folder "\" >> terraform.tfvars
+echo parent_folder =  "\"$parent_folder"\" >> terraform.tfvars
 cat terraform.tfvars
 #cd ../..
 #make org
