9173ef7ef52fa22f4e3d0cbab367ff87ad010d38
alanzoabc <67590124+alanzoabc@users.noreply.github.com>
2021-08-06 10:04:18 +0100

    Update Jenkins-org-deploy.sh

--- scripts/org/Jenkins-org-deploy.sh
+++ scripts/org/Jenkins-org-deploy.sh
@@ -8,12 +8,12 @@ echo billing_data_users ="\"$billing_data_users "\" >> terraform.tfvars
 echo audit_data_users = "\"$audit_data_users"\" >> terraform.tfvars
 echo org_id = "\"$org_id"\" >> terraform.tfvars
 echo billing_account = "\"$billing_account"\" >> terraform.tfvars
-echo terraform_service_account = "\"$terraform_service_account”\” >> terraform.tfvars
+echo terraform_service_account = "\"$terraform_service_account"\" >> terraform.tfvars
 echo default_region = "\"$default_region"\" >> terraform.tfvars
 echo scc_notification_name = "\"$scc_notification_name"\" >> terraform.tfvars
 echo create_access_context_manager_access_policy = "false" >> terraform.tfvars
 echo parent_folder =  "\"$parent_folder "\" >> terraform.tfvars
 cat terraform.tfvars
-cd ../..
+#cd ../..
 #make org
 
